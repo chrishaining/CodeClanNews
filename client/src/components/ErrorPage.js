be753eb1d8898f8a5fe93783c488886088ddf369
Chris <chrishaining@yahoo.co.uk>
2020-02-06 14:04:24 +0000

    changed error page

--- client/src/components/ErrorPage.js
+++ client/src/components/ErrorPage.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const ErrorPage = () => (
-    <img className="ErrorPage" src ="error.jpg" alt="thelads"></img>
+    <img className="ErrorPage" src ="error.jpg" alt="error"></img>
 )
 
 export default ErrorPage;
